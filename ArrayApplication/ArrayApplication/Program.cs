743d4d5328b08cae2052439a315480793dbdd8fd
Lady Gonzales <ldy.gonzales@outlook.com>
2018-02-01 14:24:32 -0500

    Primera Version By Carmen :v

---
+++ ArrayApplication/ArrayApplication/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new int[10];
+            int i, j;
+
+            for (i = 0; i < 10; i++)
+            {
+                n[i] = i + 100;
+            }
+            for (j = 0; j < 10; j++)
+            {
+                Console.WriteLine("Write j[{0}]={1}", j,n[j]);
+                
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
